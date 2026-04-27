d9c9fde97a27620c6719cb9024a4fd7894cb5ef2
tay27890123 <144402123+tay27890123@users.noreply.github.com>
2026-04-28 02:19:02 +0800

    Complete 03 data cleaning notebook and modify some feature of 02notebook

--- database/queries/state_performance.sql
+++ database/queries/state_performance.sql
@@ -1,15 +1,24 @@
 
 SELECT
-    c.customer_state                                              AS state,
-    COUNT(DISTINCT o.order_id)                                    AS total_orders,
-    ROUND(SUM(p.payment_value), 2)                                AS total_revenue,
-    ROUND(SUM(p.payment_value) / COUNT(DISTINCT o.order_id), 2)  AS revenue_per_order,
-    ROUND(AVG(r.review_score), 2)                                 AS avg_review_score
+    c.customer_state AS state,
+    COUNT(DISTINCT o.order_id) AS total_orders,
+
+    ROUND(SUM(p.payment_value), 2) AS total_revenue,
+
+    CASE
+        WHEN SUM(p.payment_value) / COUNT(DISTINCT o.order_id) >= 200 THEN 'High Value'
+        WHEN SUM(p.payment_value) / COUNT(DISTINCT o.order_id) >= 165 THEN 'Medium Value'
+        ELSE 'Low Value'
+    END AS customer_value_segment,
+
+    ROUND(SUM(p.payment_value) / COUNT(DISTINCT o.order_id), 2) AS revenue_per_order,
+    ROUND(AVG(r.review_score), 2) AS avg_review_score
+
 FROM orders o
 JOIN customers c          ON o.customer_id = c.customer_id
 JOIN order_payments p     ON o.order_id    = p.order_id
 LEFT JOIN order_reviews r ON o.order_id    = r.order_id
 WHERE o.order_status = 'delivered'
 GROUP BY state
-ORDER BY total_revenue DESC
-LIMIT 15
+ORDER BY total_revenue DESC, avg_review_score DESC
+LIMIT 15;
